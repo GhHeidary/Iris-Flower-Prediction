49091b65c2bc75ecf44e4c8e837875019cdd1c2b
GhHeidary <105490992+GhHeidary@users.noreply.github.com>
2022-05-16 21:14:34 +0200

    Update stapp.py

--- stapp.py
+++ stapp.py
@@ -2,7 +2,6 @@ from tracemalloc import Snapshot
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
 import sklearn
 from sklearn import datasets
